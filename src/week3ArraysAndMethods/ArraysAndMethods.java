40f4c984fef27c76b19abb428af419ad8dd72951
Nick Suwyn <nicholas_suwyn@suwynitsolutions.com>
2020-11-03 08:38:24 -0700

    first commit

---
+++ src/week3ArraysAndMethods/ArraysAndMethods.java
@@ -0,0 +1,57 @@
+package week3ArraysAndMethods;
+
+public class ArraysAndMethods {
+	
+	public static void main(String[] args) {
+		
+		//create an array with the following values 1, 5, 2, 8, 13, 6
+		
+		//print out the first element in the array
+		
+		//print out the last element in the array without using the number 5
+		
+		//print out the element in the array at position 6. what happens?
+		
+		//print out the element in the array at position -1. what happens?
+		
+		//write a traditional for loop that prints out each element in the array
+		
+		//write an enhanced for loop that prints out each element in the array
+		
+		//create a new variable called sum, write a loop that adds each element in the array to the sum
+		
+		//create a new variable called average and assign the average value of the array to it
+		
+		//write an enhanced for loop that prints out each number in the array only if the number is odd
+		
+		//create an array that contains the values "Sam", "Sally", "Thomas", and "Robert"
+		
+		//calculate the sum of all the letters in the new array
+		
+		
+		
+	}
+	
+	//write and test a method that takes a String name and prints out a greeting, this method returns nothing
+	
+	//write and test a method that takes a String name and returns a greeting, do not print in the method
+	
+	//analyze the difference between these two methods - what do you find? How are they different?
+	
+	//write and test a method that takes a String and an int and returns true if the number of letters in the string is greater than the int
+	
+	//write and test a method that takes an array of string and a string and returns true if the string passed in exists in the array
+	
+	//write and test a method that takes an array of int and returns the smallest number in the array
+	
+	//write and test a method that takes an array of double and returns the average
+	
+	//write and test a method that takes an array of Strings and returns an array of int where each element
+	//matches the length of the string at that position
+	
+	//write and test a method that takes an array of strings and returns true if the sum of letters for all strings with an 
+	//even amount of letters is greater than the sum of those with an odd amount of letters.
+	
+	//write and test a method that takes a string and returns true if the string is a palindrome
+
+}
---
+++ src/week4StringBuliderListSetMap/StringBuilderListSetMap.java
@@ -0,0 +1,11 @@
+package week4StringBuliderListSetMap;
+
+public class StringBuilderListSetMap {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
